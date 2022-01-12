22d78fb8ff8dff2737bc6149e9c541844481b18e
PonguinxD <95340907+PonguinxD@users.noreply.github.com>
2022-01-12 19:28:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const xhr = new XMLHttpRequest();
+xhr.open('GET', `https://apiv2.nethergames.org/v1/guilds/Oasis%20II`, true);
+xhr.onload = function () {
+    if(this.status === 200) {
+       let json = JSON.parse(this.responseText);
+       //console.log(json);
+       for (members in json){
+           console.log(json[members])
+       }
+    }
+    else{
+      console.log("some error occured")
+    }
+}
 
+xhr.send()
